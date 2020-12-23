f30ec9503b31ec0055cb1c0b58090a5df8734fda
Grigorev German <ggrigoriev.000spb@gmail.com>
2020-12-24 00:23:50 +0300

    first version

--- app.js
+++ app.js
@@ -1,41 +1,89 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const port = 3000;
+const request = require('request-promise');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.json());
+app.use(cors());
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'lab3'
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+connection.connect();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const APP_ID = '4f9daa6adb52684f45d6a42b2a313564';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+function url(path) {
+  return `${BASE_URL}${path}&appid=${APP_ID}&units=metric&lang=ru`;
+}
+
+app.get('/weather/city', (req, res) => {
+  const options = {
+    method: 'GET',
+    uri: encodeURI(url(`weather?q=${req.query.q}`)),
+  };
+
+  console.log(req.query.q);
+  request(options)
+    .then(function (response) {
+      res.send(response);
+    })
+    .catch(function (err) {
+      res.status(500).send({ error: 'Server error' });
+    });
 });
 
-module.exports = app;
+app.get('/weather/coordinates', (req, res) => {
+  const options = {
+    method: 'GET',
+    uri: url(`weather?lat=${req.query.lat}&lon=${req.query.lon}`),
+  };
+
+  request(options)
+    .then(function (response) {
+      res.send(response);
+    })
+    .catch(function (err) {
+      res.status(500).send({ error: 'Server error' });
+    });
+});
+
+// //db
+app.get('/favourites', (request, response) => {
+  connection.query('SELECT name from favourites', function (err, rows, fields) {
+    if (err) throw err;
+    response.json(rows);
+  });
+});
+
+app.post('/favourites', (request, response) => {
+  connection.query(`insert into favourites(name) values('${request.body.name}')`, function (err, rows, fields) {
+    if (err) {
+      response.status(500).send();
+    } else {
+      console.log('insertID: ', rows.insertId);
+      response.status(201).send(rows);
+    }
+  });
+});
+
+app.delete('/favourites', (request, response) => {
+  connection.query(`delete from favourites where name='${request.body.name}'`, function (err, rows, fields) {
+    if (err) throw err;
+    response.status(201).send();
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
+});
